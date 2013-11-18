f65cf132c66f821c7b84c69e03677fa9d1f594e6
mlamtahri_cp <SND\mlamtahri_cp>
2013-11-18 17:08:49 +0000

    Epi Info Web Enter:Initial source code.

---
+++ EIWS_BLL_Core/Admin.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Epi.Web.Common.BusinessObject;
+using Epi.Web.Common.Criteria;
+using System.Configuration;
+using Epi.Web.Common.Exception;
+namespace Epi.Web.BLL
+    {
+   public class Admin
+        {
+
+       public Epi.Web.Interfaces.DataInterfaces.IAdminDao AdminDao;
+
+           public Admin(Epi.Web.Interfaces.DataInterfaces.IAdminDao pAdminDao)
+            {
+               this.AdminDao = pAdminDao;
+            }
+
+            
+
+           public  List<AdminBO> GetAdminInfoByOrgKey(string gOrgKeyEncrypted)
+               {
+               string OrganizationKey = Epi.Web.Common.Security.Cryptography.Encrypt(gOrgKeyEncrypted);
+               List<AdminBO> result = this.AdminDao.GetAdminInfoByOrgKey(OrganizationKey);
+               return result;
+               }
+           public List<AdminBO> GetAdminInfoByOrgId(int OrgId)
+               {
+               List<AdminBO> result = this.AdminDao.GetAdminInfoByOrgId(OrgId);
+               return result;
+               }
+          
+           private List<AdminBO> GetOrganizationAdmins(SurveyInfoBO request)
+               {
+              
+              
+               List<AdminBO> AdminBOList = new List<AdminBO>(); 
+               try
+                   {
+
+                   Epi.Web.Interfaces.DataInterfaces.IAdminDao AdminDao = new EF.EntityAdminDao();
+                   Epi.Web.BLL.Admin Implementation = new Epi.Web.BLL.Admin(AdminDao);
+                   AdminBOList = Implementation.GetAdminInfoByOrgKey(request.OrganizationKey.ToString());
+                  
+                 
+                }
+               catch (Exception ex)
+                   {
+                   CustomFaultException customFaultException = new CustomFaultException();
+                   customFaultException.CustomMessage = ex.Message;
+                   customFaultException.Source = ex.Source;
+                   customFaultException.StackTrace = ex.StackTrace;
+                   customFaultException.HelpLink = ex.HelpLink;
+                    
+                 }
+               return AdminBOList;
+               }
+
+           public void SendEmailToAdmins(SurveyInfoBO SurveyInfo)
+               {
+
+                List<AdminBO> AdminBOList = new  List<AdminBO>();
+               List<string> AdminList = new List<string>();
+              
+               AdminBOList = GetOrganizationAdmins(SurveyInfo);
+               foreach (var item in AdminBOList)
+                   {
+                   AdminList.Add(item.AdminEmail);
+                   }
+
+               Epi.Web.Common.Email.Email Email = new Web.Common.Email.Email();
+               Email.Body = "The following survey has been promoted to FINAL mode:\n Title:" + SurveyInfo.SurveyName + " \n Survey ID:" + SurveyInfo.SurveyId + " \nOrganization:" + SurveyInfo.OrganizationName + "\n Start Date & Time:" + SurveyInfo.StartDate + "\n Closing Date & Time:" + SurveyInfo.ClosingDate + " \n \n \n  Thank you.";
+               Email.From = ConfigurationManager.AppSettings["EMAIL_FROM"];
+               Email.To = AdminList;
+               Email.Subject = "Survey -" + SurveyInfo.SurveyName + " has been promoted to FINAL";
+               bool success = Epi.Web.Common.Email.EmailHandler.SendMessage(Email);
+
+               }
+        }
+    }
---
+++ EIWS_BLL_Core/SurveyInfo.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Epi.Web.Common.BusinessObject;
+using Epi.Web.Common.Criteria;
+
+namespace Epi.Web.BLL
+{
+
+  public  class SurveyInfo
+    {
+      private Epi.Web.Interfaces.DataInterfaces.ISurveyInfoDao SurveyInfoDao;
+
+
+        public SurveyInfo(Epi.Web.Interfaces.DataInterfaces.ISurveyInfoDao pSurveyInfoDao)
+        {
+            this.SurveyInfoDao = pSurveyInfoDao;
+        }
+
+        public SurveyInfoBO GetSurveyInfoById(string pId)
+        {
+            List<string> IdList = new List<string>();
+            if (! string.IsNullOrEmpty(pId))
+            {
+                IdList.Add(pId);
+            }
+            List<SurveyInfoBO> result = this.SurveyInfoDao.GetSurveyInfo(IdList);
+            if (result.Count > 0)
+            {
+                return result[0];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+
+     
+
+        /// <summary>
+        /// Gets SurveyInfo based on criteria
+        /// </summary>
+        /// <param name="SurveyInfoId">Unique SurveyInfo identifier.</param>
+        /// <returns>SurveyInfo.</returns>
+        public List<SurveyInfoBO> GetSurveyInfoById(List<string> pIdList)
+        {
+            List<SurveyInfoBO> result = this.SurveyInfoDao.GetSurveyInfo(pIdList);
+            return result;
+        }
+
+        public PageInfoBO GetSurveySizeInfo(List<string> pIdList,int BandwidthUsageFactor, int pResponseMaxSize = -1)
+        { 
+            List<SurveyInfoBO> SurveyInfoBOList = this.SurveyInfoDao.GetSurveySizeInfo(pIdList, -1, -1, pResponseMaxSize);
+
+            PageInfoBO result = new PageInfoBO();
+
+            result = Epi.Web.BLL.Common.GetSurveySize(SurveyInfoBOList,BandwidthUsageFactor, pResponseMaxSize);
+            return result;
+
+
+        }
+
+
+        public bool IsSurveyInfoValidByOrgKeyAndPublishKey(string SurveyId, string Okey, Guid publishKey)
+        {
+
+            string EncryptedKey = Epi.Web.Common.Security.Cryptography.Encrypt(Okey);
+            List<SurveyInfoBO> result = this.SurveyInfoDao.GetSurveyInfoByOrgKeyAndPublishKey(SurveyId, EncryptedKey, publishKey);
+
+             
+            if (result != null && result.Count > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
+        public bool IsSurveyInfoValidByOrgKey(string SurveyId, string pOrganizationKey)
+        {
+
+            string EncryptedKey = Epi.Web.Common.Security.Cryptography.Encrypt(pOrganizationKey);
+            List<SurveyInfoBO> result = this.SurveyInfoDao.GetSurveyInfoByOrgKey(SurveyId, EncryptedKey);
+
+
+            if (result != null && result.Count > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
+      /// <summary>
+        /// Gets SurveyInfo based on criteria
+        /// </summary>
+        /// <param name="SurveyInfoId">Unique SurveyInfo identifier.</param>
+        /// <returns>SurveyInfo.</returns>
+        public List<SurveyInfoBO> GetSurveyInfo(List<string> SurveyInfoIdList, DateTime pClosingDate, string Okey, int pSurveyType = -1, int pPageNumber = -1, int pPageSize = -1)
+        {
+            string EncryptedKey = Epi.Web.Common.Security.Cryptography.Encrypt(Okey);
+            List<SurveyInfoBO> result = this.SurveyInfoDao.GetSurveyInfo(SurveyInfoIdList, pClosingDate, EncryptedKey, pSurveyType, pPageNumber, pPageSize);
+            return result;
+        }
+        public PageInfoBO GetSurveySizeInfo(List<string> SurveyInfoIdList, DateTime pClosingDate, string Okey, int BandwidthUsageFactor, int pSurveyType = -1, int pPageNumber = -1, int pPageSize = -1, int pResponseMaxSize = -1)
+        {
+
+            string EncryptedKey = Epi.Web.Common.Security.Cryptography.Encrypt(Okey);
+
+            List<SurveyInfoBO> SurveyInfoBOList = this.SurveyInfoDao.GetSurveySizeInfo(SurveyInfoIdList, pClosingDate, EncryptedKey, pSurveyType, pPageNumber, pPageSize, pResponseMaxSize);
+
+            PageInfoBO result = new PageInfoBO();
+
+            result = Epi.Web.BLL.Common.GetSurveySize(SurveyInfoBOList, BandwidthUsageFactor, pResponseMaxSize);
+            return result;
+
+        }
+      
+        public SurveyInfoBO InsertSurveyInfo(SurveyInfoBO pValue)
+        {
+            SurveyInfoBO result = pValue;
+            this.SurveyInfoDao.InsertSurveyInfo(pValue);
+            return result;
+        }
+
+
+
+        public SurveyInfoBO UpdateSurveyInfo(SurveyInfoBO pValue)
+        {
+            SurveyInfoBO result = pValue;
+            if (ValidateSurveyFields(pValue))
+            {
+                this.SurveyInfoDao.UpdateSurveyInfo(pValue);
+                result.StatusText = "Successfully updated survey information.";
+            }else{
+                result.StatusText = "One or more survey required fields are missing values.";
+            
+            }
+            
+            return result;
+        }
+
+        public bool DeleteSurveyInfo(SurveyInfoBO pValue)
+        {
+            bool result = false;
+
+            this.SurveyInfoDao.DeleteSurveyInfo(pValue);
+            result = true;
+
+            return result;
+        }
+        private static bool ValidateSurveyFields(SurveyInfoBO pRequestMessage)
+        {
+
+            bool isValid = true;
+
+
+            if (pRequestMessage.ClosingDate == null)
+            {
+
+                isValid = false;
+
+            }
+
+           
+            else if (string.IsNullOrEmpty(pRequestMessage.SurveyName))
+            {
+
+                isValid = false;
+            }
+ 
+
+
+
+            return isValid;
+        }
+     
+    }
+}
