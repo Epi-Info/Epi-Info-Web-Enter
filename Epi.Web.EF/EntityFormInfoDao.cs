c9ac2164f9de4b9d37bb511ab2372f8496f68f6f
mlamtahri_cp <24397119+mlamtahri@users.noreply.github.com>
2013-11-20 13:49:46 +0000

    EWE: Added a new query to EF to read all Form

---
+++ Epi.Web.EF/EntityFormInfoDao.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Linq;
+using System.Text;
+using Epi.Web.Common.BusinessObject;
+using Epi.Web.Common.Criteria;
+using Epi.Web.Interfaces.DataInterface;
+namespace Epi.Web.EF
+    {
+    public class EntityFormInfoDao: IFormInfoDao
+        {
+        public List<FormInfoBO> GetFormInfo(Guid UserId) {
+        List<FormInfoBO> FormList = new List<FormInfoBO>();
+
+     
+            try
+                {
+
+                Guid Id = UserId;
+
+                    using (var Context = DataObjectFactory.CreateContext())
+                        {
+                        FormList.Add(Mapper.MapToFormInfoBO(Context.SurveyMetaDatas.FirstOrDefault(x => x.SurveyId == Id)));
+                        }
+                
+                }
+            catch (Exception ex)
+                {
+                throw (ex);
+                }
+           
+
+
+
+        return FormList;
+            }
+        }
+    }
