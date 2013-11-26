c3f21ee615fe4d54921e3385841c9e0325d58cac
mlamtahri_cp <SND\mlamtahri_cp>
2013-11-26 16:55:02 +0000

    EWE : Added new Objects for reading survey Response data.

---
+++ Epi.Web.Common/Message/FormResponseInfoRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Epi.Web.Common.MessageBase;
+using Epi.Web.Common.DTO;
+
+namespace Epi.Web.Common.Message
+    {
+     [DataContract(Namespace = "http://www.yourcompany.com/types/")]
+    public class FormResponseInfoRequest
+        {
+        }
+    }
