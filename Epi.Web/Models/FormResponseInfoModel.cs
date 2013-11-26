34486563979f079f0af5dc7f97d3dd0f91910639
mausman_cp <SND\mausman_cp>
2013-11-26 16:27:45 +0000

    EWE:- Adding FormResponseInfoModel, Updating jquery

---
+++ Epi.Web/Models/FormResponseInfoModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Epi.Web.MVC.Models
+{
+    public class FormResponseInfoModel
+    {
+        public List<ResponseInfoModel> ResponseInfoModels { get; set; }
+        public FormInfoModel FormInfoModel { get; set; }
+    }
+}
---
+++ Epi.Web/Models/ResponseInfoModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Epi.Web.MVC.Models
+{
+    public class ResponseInfoModel
+    {
+        private string _ResponseId;
+        public string ResponseId
+        { get; set; }
+    }
+}
