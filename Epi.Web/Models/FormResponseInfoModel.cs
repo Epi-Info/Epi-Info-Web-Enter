13790719e1ea9f97e94b745b168687fde21cb7c5
mausman_cp <SND\mausman_cp>
2013-12-12 19:26:27 +0000

    EWE:- Added logic for dynamic columns names, limit stays upto to 5 columns. Added IsLocked functionality.

--- Epi.Web/Models/FormResponseInfoModel.cs
+++ Epi.Web/Models/FormResponseInfoModel.cs
@@ -10,11 +10,16 @@ namespace Epi.Web.MVC.Models
         public FormInfoModel FormInfoModel;
         public List<ResponseModel> ResponsesList;
         public List<string> ColumnNames;
+        public List<KeyValuePair<int, string>> Columns;
+
+
         public FormResponseInfoModel()
         {
             FormInfoModel = new FormInfoModel();
             ResponsesList = new List<ResponseModel>();
             ColumnNames = new List<string>();
+            Columns = new List<KeyValuePair<int, string>>();
         }
     }
+
 }
