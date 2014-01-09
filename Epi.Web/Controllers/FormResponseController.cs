464df2424270501629e0df2908ece05bacc834e9
mlamtahri_cp <24397119+mlamtahri@users.noreply.github.com>
2014-01-09 19:49:58 +0000

    EWE: Added logic to show the right message when clicking Exit button.

--- Epi.Web/Controllers/FormResponseController.cs
+++ Epi.Web/Controllers/FormResponseController.cs
@@ -177,7 +177,7 @@ namespace Epi.Web.MVC.Controllers
             Epi.Web.Common.DTO.SurveyAnswerDTO surveyAnswerDTO = GetSurveyAnswer(EditForm);
             surveyAnswerDTO.Status = 1;
             string ChildRecordId = GetChildRecordId(surveyAnswerDTO);
-            return RedirectToAction(Epi.Web.MVC.Constants.Constant.INDEX, Epi.Web.MVC.Constants.Constant.SURVEY_CONTROLLER, new { responseid = ChildRecordId, PageNumber = 1 });
+            return RedirectToAction(Epi.Web.MVC.Constants.Constant.INDEX, Epi.Web.MVC.Constants.Constant.SURVEY_CONTROLLER, new { responseid = ChildRecordId, PageNumber = 1,Edit ="Edit" });
             }
             bool IsMobileDevice = this.Request.Browser.IsMobileDevice;
 
