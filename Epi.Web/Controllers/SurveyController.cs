994c71d0f37adcbc17e05ee770453960900b3e81
mlamtahri_cp <24397119+mlamtahri@users.noreply.github.com>
2014-06-03 14:32:13 +0000

    EWE: I fixed issue related to radio button not holding value when saving and closing a form.

--- Epi.Web/Controllers/SurveyController.cs
+++ Epi.Web/Controllers/SurveyController.cs
@@ -407,30 +407,12 @@ namespace Epi.Web.MVC.Controllers
                               
                                    
                                     foreach (var Obj in List)
-                                        {
-                                        // SurveyAnswerDTO SurveyAnswer2 = _isurveyFacade.GetSurveyAnswerResponse(RootResponseId).SurveyResponseList[0];
-                                        //  SurveyInfoModel surveyInfoModel2 = GetSurveyInfo(RootFormId);
-
-
-
-                                        SurveyAnswerDTO SurveyAnswer2 = _isurveyFacade.GetSurveyAnswerResponse(Obj.ResponseId).SurveyResponseList[0];
-                                        SurveyInfoModel surveyInfoModel2 = GetSurveyInfo(Obj.SurveyId);
-
-                                        MvcDynamicForms.Form form2 = UpDateSurveyModel(surveyInfoModel2, IsMobileDevice, FormValuesHasChanged, SurveyAnswer2, true);
-                                        if (!IsMobileDevice)
-                                            {
-                                            _isurveyFacade.UpdateSurveyResponse(surveyInfoModel2, Obj.ResponseId, form2, SurveyAnswer2, IsSubmited, true, PageNumber, UserId);
-                                            }
-                                        else {
-                                            if (!IsEditMode)
-                                                {
-                                                _isurveyFacade.UpdateSurveyResponse(surveyInfoModel2, Obj.ResponseId, form2, SurveyAnswer2, IsSubmited, true, PageNumber, UserId);
-                                                }
-                                            
-                                            }
-                                        SurveyAnswerRequest1.SurveyAnswerList.Add(SurveyAnswer2);
-
-                                        }
+                                      {
+                                    
+                                       SurveyAnswerDTO SurveyAnswer2 = _isurveyFacade.GetSurveyAnswerResponse(Obj.ResponseId).SurveyResponseList[0];
+                                       SurveyInfoModel surveyInfoModel2 = GetSurveyInfo(Obj.SurveyId);
+                                       SurveyAnswerRequest1.SurveyAnswerList.Add(SurveyAnswer2);
+                                      }
                                      
                                 if(this.IsEditMode){
                                 _isurveyFacade.DeleteResponseXml(SurveyAnswerRequest1);
