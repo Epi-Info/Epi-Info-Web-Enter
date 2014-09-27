0feb56ffc4f2381a2fd164c07e7af210506872f8
mausman_cp <SND\mausman_cp>
2014-09-27 15:15:59 +0000

    EWE:- Adding IsSqlProject flag for UI

--- Epi.Web.Common/DTO/FormInfoDTO.cs
+++ Epi.Web.Common/DTO/FormInfoDTO.cs
@@ -5,10 +5,10 @@ using System.Text;
 using System.Runtime.Serialization;
 
 namespace Epi.Web.Enter.Common.DTO
-    {
-     [DataContract(Namespace = "http://www.yourcompany.com/types/")]
+{
+    [DataContract(Namespace = "http://www.yourcompany.com/types/")]
     public class FormInfoDTO
-        {
+    {
         private string _FormId;
         private string _FormNumber;
         private string _FormName;
@@ -19,67 +19,75 @@ namespace Epi.Web.Enter.Common.DTO
         private bool _IsOwner;
         private string _OwnerLName;
         private string _OwnerFName;
+        private bool _isSQLProject;
         [DataMember]
         public string FormId
-            {
+        {
             get { return _FormId; }
             set { _FormId = value; }
-            }
+        }
         [DataMember]
         public string FormNumber
-            {
+        {
             get { return _FormNumber; }
             set { _FormNumber = value; }
-            }
+        }
 
         [DataMember]
         public string FormName
-            {
+        {
             get { return _FormName; }
             set { _FormName = value; }
-            }
+        }
         [DataMember]
         public string OrganizationName
-            {
+        {
             get { return _OrganizationName; }
             set { _OrganizationName = value; }
-            }
+        }
         [DataMember]
         public int OrganizationId
-            {
+        {
             get { return _OrganizationId; }
             set { _OrganizationId = value; }
-            }
-         [DataMember]
+        }
+        [DataMember]
         public bool IsDraftMode
-            {
+        {
             get { return _IsDraftMode; }
             set { _IsDraftMode = value; }
-            }
+        }
 
         [DataMember]
         public int UserId
-            {
+        {
             get { return _UserId; }
             set { _UserId = value; }
-            }
-       [DataMember]
+        }
+        [DataMember]
         public bool IsOwner
-            {
+        {
             get { return _IsOwner; }
             set { _IsOwner = value; }
-            }
-       [DataMember]
-       public string OwnerLName
-           {
-           get { return _OwnerLName; }
-           set { _OwnerLName = value; }
-           }
-       [DataMember]
-       public string OwnerFName
-           {
-           get { return _OwnerFName; }
-           set { _OwnerFName = value; }
-           }
+        }
+        [DataMember]
+        public string OwnerLName
+        {
+            get { return _OwnerLName; }
+            set { _OwnerLName = value; }
+        }
+        [DataMember]
+        public string OwnerFName
+        {
+            get { return _OwnerFName; }
+            set { _OwnerFName = value; }
+        }
+
+        [DataMember]
+        public bool IsSQLProject
+        {
+            get { return _isSQLProject; }
+            set { _isSQLProject = value; }
         }
     }
+}
