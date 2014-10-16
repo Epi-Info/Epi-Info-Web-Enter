2a2ca6f2ea100f2276981de8067ca85ab5ec43d0
mlamtahri_cp <SND\mlamtahri_cp>
2014-10-16 00:28:13 +0000

    EWE: Bug Fix

--- Epi.Web/Controllers/SurveyController.cs
+++ Epi.Web/Controllers/SurveyController.cs
@@ -869,7 +869,7 @@ namespace Epi.Web.MVC.Controllers
             SARequest.SurveyAnswerList.Add(new SurveyAnswerDTO() { ResponseId = ResponseId });
             SARequest.Criteria.UserId = SurveyHelper.GetDecryptUserId(Session["UserId"].ToString());
             SARequest.Criteria.IsEditMode = false;
-            SARequest.Criteria.IsDeleteMode = true;
+            SARequest.Criteria.IsDeleteMode = false;
             SARequest.Criteria.IsSqlProject = (bool)Session["IsSqlProject"];
             SARequest.Criteria.SurveyId = Session["RootFormId"].ToString();
             SurveyAnswerResponse SAResponse = _isurveyFacade.DeleteResponse(SARequest);
