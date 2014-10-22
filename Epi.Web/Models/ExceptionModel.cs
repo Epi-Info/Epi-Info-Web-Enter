7164b38a88ea15802ed1857222cc04412842779f
mausman_cp <SND\mausman_cp>
2014-10-22 19:46:13 +0000

    EWE:- Adding exceptionModel

---
+++ Epi.Web/Models/ExceptionModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Epi.Web.MVC.Models
+{
+    public class ExceptionModel
+    {
+        public string ExceptionDetail { get; set; }
+        public string Message { get; set; }
+
+    }
+}
