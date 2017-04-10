9fc90905e2601d1de744b46f6f520d134a44cda0
mlamtahri_cp <SND\mlamtahri_cp>
2017-04-10 17:49:37 +0000

    EW-349

--- Epi.DynamicForms.Core/Fields/CheckBox.cs
+++ Epi.DynamicForms.Core/Fields/CheckBox.cs
@@ -160,7 +160,7 @@ namespace MvcDynamicForms.Fields
             prompt.Attributes.Add("class", "EpiLabel");
             prompt.Attributes.Add("Id", "label" + inputName);
             StringBuilder StyleValues = new StringBuilder();
-            StyleValues.Append(GetContolStyle(_fontstyle.ToString(), _Prompttop.ToString(), _Promptleft.ToString(), null, Height.ToString(), IsHidden));
+            StyleValues.Append(GetContolStyle(_fontstyle.ToString(), _Prompttop.ToString(), _Promptleft.ToString(), null,null, IsHidden));
             prompt.Attributes.Add("style", StyleValues.ToString());
             html.Append(prompt.ToString());
             if (ReadOnly || _IsDisabled)
